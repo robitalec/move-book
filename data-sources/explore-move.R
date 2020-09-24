7643b59a16c6bcb409f506701e40059444cfbce9
Alec L. Robitaille <robit.alec@gmail.com>
2020-09-24 14:54:02 -0230

    use keyring package

--- data-sources/explore-move.R
+++ data-sources/explore-move.R
@@ -6,6 +6,12 @@ install.packages('move')
 library(move)
 
 
-# Get all the studies
+# Credentials
+install.packages("keyring")
 
+keyring::key_set(service = "movebank",
+								 username = "robitalec")
+
+
+# Get all the studies
 studies <- getMovebankStudies()
