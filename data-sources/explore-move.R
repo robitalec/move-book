beb38b371639e45d7b1aa3f3d61d782eb7a1b55e
Alec L. Robitaille <robit.alec@gmail.com>
2020-09-24 15:02:16 -0230

    start scraping all of movebank

--- data-sources/explore-move.R
+++ data-sources/explore-move.R
@@ -7,6 +7,7 @@
 # install.packages('move')
 library(move)
 
+library(data.table)
 
 
 # Credentials -------------------------------------------------------------
@@ -26,3 +27,10 @@ login <- movebankLogin(key_list(service)[1, 2], key_get(service, username))
 
 # Get all the studies
 studies <- getMovebankStudies(login)
+
+# Get details
+details <- rbindlist(lapply(studies, getMovebankStudy, login = login))
+
+# Movebank has no automated/complete database to download
+# Therefore, we'll loop through all the studies, get the details,
+# and summarize how much data is actually accessible
