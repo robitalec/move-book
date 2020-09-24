04524f1c446855800650767888f1a5147ca9b132
Alec L. Robitaille <robit.alec@gmail.com>
2020-09-24 15:11:59 -0230

    more than one study with name

--- data-sources/explore-move.R
+++ data-sources/explore-move.R
@@ -28,9 +28,14 @@ login <- movebankLogin(key_list(service)[1, 2], key_get(service, username))
 # Get all the studies
 studies <- getMovebankStudies(login)
 
-# Get details
-details <- rbindlist(lapply(studies, getMovebankStudy, login = login))
-
 # Movebank has no automated/complete database to download
 # Therefore, we'll loop through all the studies, get the details,
 # and summarize how much data is actually accessible
+details <- rbindlist(lapply(studies, getMovebankStudy, login = login))
+
+# Error: There was more than one study with the name: X
+# How many times does this occur? 17 unique studies, 88 rows (2020-09-04)
+morethanone <- data.table(study = studies)[, .N, by = study][N > 1]
+
+substudies <- studies[!studies %in% morethanone$study]
+
