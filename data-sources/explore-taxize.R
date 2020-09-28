f490d16fc86a8dd93d7a24de99a2cf087a34b526
Alec L. Robitaille <robit.alec@gmail.com>
2020-09-28 13:51:28 -0230

    fst gnr resolve taxon ids

--- data-sources/explore-taxize.R
+++ data-sources/explore-taxize.R
@@ -9,7 +9,11 @@ library(taxize)
 
 
 # Data --------------------------------------------------------------------
+details <- fread('data-sources/details.csv')
 
 
 
-
+# Taxize ------------------------------------------------------------------
+# First resolve the submitted name
+resolved <- gnr_resolve(details$taxon_ids)
+# details[, resolved := list(gnr_resolve(taxon_ids))]
