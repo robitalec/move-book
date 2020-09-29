241d76d67ac7037dd73956745ade18df7f30287a
Alec L. Robitaille <robit.alec@gmail.com>
2020-09-29 13:27:31 -0230

    check na null "" for taxon_ids
    0 NA
    0 NULL
    1644 ""

--- data-sources/explore-taxize.R
+++ data-sources/explore-taxize.R
@@ -14,6 +14,13 @@ details <- fread('data-sources/details.csv')
 
 
 # Taxize ------------------------------------------------------------------
+details[is.null(taxon_ids), .N]
+details[is.na(taxon_ids), .N]
+details[taxon_ids == "", .N]
+
+
 # First resolve the submitted name
-resolved <- gnr_resolve(details$taxon_ids)
+resolved <- gnr_resolve(details$taxon_ids, best_match_only = TRUE)
+
+setDT(resolved)
 # details[, resolved := list(gnr_resolve(taxon_ids))]
