e87af830c68e2bdb9a274d3bc346f7d4a3991a41
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-01 16:08:39 -0230

    fst explore rmoveapi

---
+++ data-sources/explore-rmoveapi.R
@@ -0,0 +1,12 @@
+# === Explore rmoveapi ----------------------------------------------------
+# Alec Robitaille
+
+
+# Packages ----------------------------------------------------------------
+# remotes::install_github('benscarlson/rmoveapi')
+library(rmoveapi)
+
+
+# Licenses ----------------------------------------------------------------
+
+
