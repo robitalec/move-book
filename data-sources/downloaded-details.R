ff108cfad0699b5e2ea9b059b98f13fda3fb3668
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-04 16:02:10 -0230

    more taxize options

--- data-sources/downloaded-details.R
+++ data-sources/downloaded-details.R
@@ -5,15 +5,28 @@
 
 # Packages ----------------------------------------------------------------
 library(data.table)
-
+library(taxizt)
 
 # Data --------------------------------------------------------------------
 details <- fread('data-sources/details.csv')
 
+# Downloaded data
 paths <- dir('/media/Backup Plus/Movebank/GPS', full.names = TRUE)
 size <- vapply(paths, file.size, 1)
 nm <- as.integer(gsub('.csv', '', tstrsplit(paths, 'GPS/')[[2]]))
 
 DT <- data.table(size, id = nm)
 
+# Merge
 down <- merge(DT, details, on = 'id')
+
+
+# Summary -----------------------------------------------------------------
+# Quick taxon family for first char listed in taxon_ids
+# TODO: use these everywhere, more options for taxize
+down[, family := tax_name(tstrsplit(taxon_ids, ',')[[1]],
+													'family',
+													messages = FALSE,
+													ask = FALSE,
+													accepted = TRUE)$family]
+
