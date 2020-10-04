d4a88a84fb537353e3e90f17971dc12d5344d31f
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-04 16:27:43 -0230

    compare to expected, first rbindlist all downloaded

--- data-sources/downloaded-details.R
+++ data-sources/downloaded-details.R
@@ -5,6 +5,7 @@
 
 # Packages ----------------------------------------------------------------
 library(data.table)
+library(ggplot2)
 
 # Data --------------------------------------------------------------------
 details <- fread('data-sources/details.csv')
@@ -18,10 +19,25 @@ nm <- as.integer(gsub('.csv', '', tstrsplit(paths, 'GPS/')[[2]]))
 downloaded <- data.table(size, id = nm)
 
 # Merge
-withdetails <- merge(DT, details, on = 'id')
+withdetails <- merge(downloaded, details, on = 'id')
 
 
 # Summary -----------------------------------------------------------------
+# Expected
+gps <- unique(taxed, by = 'id')[
+	grepl('GPS', sensor_type_ids) & (i_have_download_access)]
+
+# Actually downloaded
 DT <- merge(withdetails,
 						unique(taxed, by = 'id')[, .(id, class, family)],
 						on = 'id')
+
+sub <- DT[!(family == '' | class == '')]
+
+sub[, qplot(family) + facet_wrap(~class)]
+
+
+
+# Combine -----------------------------------------------------------------
+zzz <- rbindlist(lapply(paths, fread),
+								 use.names = TRUE, fill = TRUE)
