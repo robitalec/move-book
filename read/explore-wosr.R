5bc5a359a2f0b61d0b7bf1b39cc0b362e09ea2e6
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-05 15:44:58 -0230

    wos credentials

--- read/explore-wosr.R
+++ read/explore-wosr.R
@@ -8,3 +8,17 @@ library(wosr)
 
 library(keyring)
 
+
+# Credentials -------------------------------------------------------------
+service = 'wosr'
+username = 'robit.alec@gmail.com'
+
+# key_set(service = service, username = username)
+
+Sys.setenv(WOS_USERNAME = username, WOS_PASSWORD = key_get(service, username))
+
+
+
+# Query -------------------------------------------------------------------
+
+
