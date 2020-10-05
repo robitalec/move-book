d7ba64d0c9fdd315e7a03c59f3767cf4c250fe67
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-05 15:58:49 -0230

    rm wosr
    requires premium api access

--- read/explore-wosr.R
+++ read/explore-wosr.R
@@ -1,24 +1,14 @@
-# Explore wosr, etc bib pkgs ----------------------------------------------
+# === Explore bib pkgs ----------------------------------------------------
 # Alec Robitaille
 
 
 # Packages ----------------------------------------------------------------
-# install.packages("wosr")
-library(wosr)
-
 library(keyring)
 
 
 # Credentials -------------------------------------------------------------
-service = 'wosr'
 username = 'robit.alec@gmail.com'
 
 # key_set(service = service, username = username)
 
-Sys.setenv(WOS_USERNAME = username, WOS_PASSWORD = key_get(service, username))
-
-
-
 # Query -------------------------------------------------------------------
-
-
