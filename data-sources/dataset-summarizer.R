f11e2c4279bfd8a2f64a0a6370ceed5dd05b3491
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-07 10:44:27 -0230

    fst dataset summarizer

---
+++ data-sources/dataset-summarizer.R
@@ -0,0 +1,37 @@
+# === Movement dataset summarizer -----------------------------------------
+# Alec Robitaille
+
+
+# Packages ----------------------------------------------------------------
+
+
+# Input -------------------------------------------------------------------
+
+
+
+# Prep --------------------------------------------------------------------
+
+
+
+# Time (Donuts) -----------------------------------------------------------
+
+
+
+# Space is only time you can see ------------------------------------------
+
+
+
+
+# Append output -----------------------------------------------------------
+
+
+
+
+
+# No point rbindlist + freading them all
+# TODO: write single input script to
+#       taxon, number of individuals, is it actually GPS?
+#       drop Argos, number of years, number of locs etc
+#       write to summary csv, keep appending
+#       bash script Rscript run on all in folder
+#       and preserve license terms etc
