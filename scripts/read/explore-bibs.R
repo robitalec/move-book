e89a53a069e599540ababbf91a47f3cd86d5476a
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-08 13:18:41 -0230

    move read to scripts/read

---
+++ scripts/read/explore-bibs.R
@@ -0,0 +1,14 @@
+# === Explore bib pkgs ----------------------------------------------------
+# Alec Robitaille
+
+
+# Packages ----------------------------------------------------------------
+library(keyring)
+
+
+# Credentials -------------------------------------------------------------
+username = 'robit.alec@gmail.com'
+
+# key_set(service = service, username = username)
+
+# Query -------------------------------------------------------------------
