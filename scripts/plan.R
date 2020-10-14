b496346f35233935a2e0dadd4b51136f456d800a
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-14 11:03:37 -0700

    packages into plan

--- scripts/plan.R
+++ scripts/plan.R
@@ -2,7 +2,19 @@
 library(drake)
 
 
+# Packages ----------------------------------------------------------------
+library(data.table)
+library(anytime)
+library(ggplot2)
+library(mapview)
+
 # Source functions
+source('scripts/summarizer/summarizer-functions.R')
+
+# Plan
+fp <- '/media/Backup Plus/Movebank/GPS'
 
 
-drake_plan()
+drake_plan(
+  checked = rbindlist(lapply(dir(fp, '.csv', full.names = TRUE), check_input))
+)
