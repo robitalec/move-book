3f49e1910870e73056140ab149fffb81f12d49d7
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-16 10:40:47 -0700

    back to dynamic
    seems much easier, tidier

--- scripts/plan.R
+++ scripts/plan.R
@@ -15,14 +15,14 @@ source('scripts/summarizer/summarizer-functions.R')
 fp <- '/media/Backup Plus/Movebank/GPS'
 
 sub <- 20:30
-paths <- dir(fp, '.csv', full.names = TRUE)[sub]
-names(paths) <- dir(fp, '.csv')[sub]
+# names(paths) <- dir(fp, '.csv')[sub]
 
 plan <- drake_plan(
-	checked = target(check_input(file_in(file)),
-									 transform = map(file = !!paths)),
+	paths = dir(fp, '.csv', full.names = TRUE)[sub],
+	checked = target(check_input(paths),
+									 dynamic = map(paths)),
 	read = target(read_input(checked),
-								transform = map(checked)),
+								dynamic = map(checked)),
 	counted = target(count_ids(read),
-									 transform = map(read))
+									 dynamic = map(read)),
 )
