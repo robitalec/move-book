aee9ad7eb3511cd4f4b999a04079d5d8475cca13
Alec L. Robitaille <robit.alec@gmail.com>
2020-11-16 19:12:51 -0330

    set cache path too

--- _drake.R
+++ _drake.R
@@ -1,5 +1,10 @@
 source("scripts/plan.R")
 
 
-cache <- new_cache('/media/ICEAGE/drake-cache/')
+if (.Platform$OS.type == "windows") {
+	cache <- new_cache(file.path('E:', 'ALR_C2', 'drake-cache'))
+} else if (.Platform$OS.type == "unix") {
+	cache <- new_cache(file.path('/media', 'ICEAGE', 'drake-cache'))
+}
+
 drake_config(plan, cache = cache)
